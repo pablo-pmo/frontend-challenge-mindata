cb7459a92f178d80fec8e0e23068dfda695e7d81
pablo-pmo <impablopmo@gmail.com>
2024-11-20 16:32:35 +0100

    feat: add basic routing

--- frontend-challenge/src/app/app.routes.ts
+++ frontend-challenge/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'heroes', component: HeroListComponent },
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+];
