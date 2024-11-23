2cc5f324c9ef29fdc8690230b5951ec6ce788319
pablo-pmo <impablopmo@gmail.com>
2024-11-23 21:52:48 +0100

    build: set up animations provider for component unit test

--- frontend-challenge/src/app/components/hero-list/hero-list.component.spec.ts
+++ frontend-challenge/src/app/components/hero-list/hero-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroListComponent } from './hero-list.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('HeroListComponent', () => {
   let component: HeroListComponent;
@@ -8,9 +9,9 @@ describe('HeroListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroListComponent]
-    })
-    .compileComponents();
+      imports: [HeroListComponent],
+      providers: [provideAnimationsAsync()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeroListComponent);
     component = fixture.componentInstance;
