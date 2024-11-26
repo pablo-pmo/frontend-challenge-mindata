d09b1908d195219b7dbbfe5c3a9111f1b8ae6072
pablo-pmo <impablopmo@gmail.com>
2024-11-26 10:59:34 +0100

    feat: created base hero-form component

--- frontend-challenge/src/app/app.routes.ts
+++ frontend-challenge/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeroListComponent } from './components/hero-list/hero-list.component';
-import { AddHeroComponent } from './components/add-hero/add-hero.component';
+import { AddHeroComponent } from './pages/add-hero/add-hero.component';
+import { EditHeroComponent } from './pages/edit-hero/edit-hero.component';
 
 export const routes: Routes = [
   { path: 'heroes', component: HeroListComponent },
   { path: 'heroes/add/:id', component: AddHeroComponent },
+  { path: 'heroes/edit/:id', component: EditHeroComponent },
   { path: '', redirectTo: 'heroes', pathMatch: 'full' },
 ];
---
+++ frontend-challenge/src/app/pages/edit-hero/edit-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-hero',
+  imports: [],
+  templateUrl: './edit-hero.component.html',
+  styleUrl: './edit-hero.component.scss'
+})
+export class EditHeroComponent {
+
+}
